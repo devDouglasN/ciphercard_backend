1858aec3e86b2ce8368b105b10f207d8819c928b
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-02-10 16:47:32 -0300

    Initial commit

---
+++ src/main/java/io/douglas/ciphercard/CipherCardApplication.java
@@ -0,0 +1,13 @@
+package io.douglas.ciphercard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CipherCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CipherCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/douglas/ciphercard/CipherCardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.douglas.ciphercard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CipherCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
